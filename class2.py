13297e8a38c24a0dc068c8f62d1847c0b7dc81f1
Student <Student@Train11-5-PC114.control.ad.sit.kmutt.ac.th>
2025-07-22 15:25:10 +0700

    Add class2

---
+++ class2.py
@@ -0,0 +1,66 @@
+# # age = 18
+# # print(age)
+
+
+# num = 5
+# print(type(num))
+
+# num2 = 1.1
+# print(type(num2))
+
+
+# boolean = True
+# print(type(boolean))
+
+# name = "kao 123"
+# print(type(name))
+
+# print(name)
+
+# age = "18"
+# print(type(age))
+
+# age2 = int(age)
+# print(type(age2))
+
+# num1 = 5
+# print(type(num1))
+
+# num2 = float(num1)
+# print(type(num2))
+# print(num2)
+
+
+# age = 18.5
+# print(type(age))
+
+# age2 = str(age)
+# print(type(age2))
+
+# tf = True
+# print(type(tf))
+
+# tf2 = str(tf)
+# print(type(tf2))
+
+# sum = "str" < "strr"
+# print(sum)
+
+
+# age = input("Enter your age.")
+# # print("your age is", age)
+# print(type(age))
+
+# name = input ("Enter your name")
+# print("your name is", name)
+
+
+age = int(input("Enter your age."))
+
+if age > 18:
+    print("Your age more than 18.")
+elif age > 20:
+    print("Your age more than 20.")
+else:
+    print("Your age lower than 18")
+
