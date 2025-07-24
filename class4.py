2e48a3542d56c8cbcd27eb3395bae0ab6744624b
phuttikorn <kaizaszizi@gmail.com>
2025-07-24 12:00:39 +0700

    class_4

---
+++ class4.py
@@ -0,0 +1,69 @@
+# # def grade(score):
+# #     if score >= 80:
+# #         print("Grade A")
+# #     elif score >= 70:
+# #         print("Grade B")
+# #     elif score >= 60:
+# #         print("Grade C")
+
+# # grade(60)
+
+# def my_sum(a, b):
+#     return(a + b)
+
+# print(my_sum(5, 5))
+
+# result = my_sum(5, 5)
+# print(result)
+# def Menu():
+#     while True:
+#         Choice = int(input("1.เลือกคำนวณราคาสินค้า , 2.ออก"))
+#         if Choice == 1:
+#             Menu = float(input("เลือกสินค้า"))
+        
+#             Type = float(input("ราคาสินค้า"))
+
+#             print(Calter(Menu, Type))
+#         elif Choice == 2:
+#             print("ออก")
+#             break
+# def Calter(a, b):
+#     return (a * b)*1.07
+
+
+# Menu()
+
+
+
+
+# print(my_list)
+
+
+# my_list[0] = 3
+# print(my_list)
+
+# print("Before", my_list)
+
+# # my_list.append("KKK")
+# my_list.pop(0)
+
+# print("After", my_list)
+
+# my_list = [1, 2, "kao"]
+
+# for i in my_list:
+#     print(i)
+
+# my_dict = {"name": "kao", "age": 20}
+# print("Before", my_dict)
+
+# my_dict["name"] = "kan"
+# print("After", my_dict)
+
+# student = [
+#     {"name": "kao", "age": 18},
+#     {"name": "P'Gap", "age": 20}
+# ]
+
+# for student in student:
+#     print(student["age"])
